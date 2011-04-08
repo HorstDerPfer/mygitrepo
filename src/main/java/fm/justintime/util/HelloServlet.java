407cff839487c3842e77494b4139064e12ed748a
Felix Michels <felix.michels@accel-gmbh.de>
2011-04-08 23:33:48 +0200

    Projekt erstellt

---
+++ src/main/java/fm/justintime/util/HelloServlet.java
@@ -0,0 +1,31 @@
+package fm.justintime.util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -154548832429822966L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+        out.println("<title>Hola</title>");
+        out.println("</head>");
+        out.println("<body bgcolor=\"white\">");
+        out.println("<h1> Hi </h1>");
+        out.println("</body>");
+		out.println("</html>");
+	}
+}
